cc73a8acb6d5f47ea5508d3cc9a58683a349b38c
Mathieu STUMPF-GUNTZ <mathieu@culture-libre.org>
2026-07-14 20:12:52 +0200

    Ĝisdatigi aŭtorrajtajn menciojn kaj versian standardon

--- fontaro/lua.h
+++ fontaro/lua.h
@@ -23,7 +23,9 @@
 
 #define LUA_VERSION	"Lua " LUA_VERSION_MAJOR "." LUA_VERSION_MINOR
 #define LUA_RELEASE	LUA_VERSION "." LUA_VERSION_RELEASE
-#define LUA_COPYRIGHT	LUA_RELEASE "  Copyright (C) 1994-2016 Lua.org, PUC-Rio"
+#define LUPA_COPYRIGHT	"Lupa 0.0.1  Aŭtorrajto (C) ekde 2016 fare de psychoslave"
+#define LUA_COPYRIGHT	LUA_RELEASE "  Copyright (C) 1994-2016 Lua.org, PUC-Rio" "\n" \
+      LUPA_COPYRIGHT
 #define LUA_AUTHORS	"R. Ierusalimschy, L. H. de Figueiredo, W. Celes"
 
 
